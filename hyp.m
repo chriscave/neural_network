adc4348873e92953d39efc2ecae28dfac82f42c8
chris <chriscave89@gmail.com>
2018-06-11 11:33:13 +0200

    returns the value of each node in the neural network. The activation function is the sigmoid function

---
+++ hyp.m
@@ -0,0 +1,15 @@
+function H = hyp(X, theta);
+  [rowct,colct] = decompose_blk_diag(theta);
+  [n,m] = size(X);
+  X = [ones(n,1),X];
+  r = size(rowct,2);
+  H = [];
+  activation = X';
+  for i = 1: (r - 2);
+    weights = theta((rowct(i):rowct(i+1) - 1), (colct(i):colct(i+1) - 1));
+    activation = [ones(1,size(X,1));sigmoid(weights * activation)];
+    H = [H;activation];
+  end
+  weights = theta((rowct(r - 1): rowct(r) - 1), ( colct(r - 1): colct(r) - 1));
+  H = [H;sigmoid(weights * activation)];
+  
